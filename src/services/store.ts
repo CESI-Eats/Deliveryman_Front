fb95177416ab4becc43275603c9a97887ea1e1af
Armand DEBESSE <73818074+armanddebesse@users.noreply.github.com>
2023-06-29 18:23:03 +0200

    [ADD] Websocket reconnection on reload (#8)

--- src/services/store.ts
+++ src/services/store.ts
@@ -76,11 +76,11 @@ export const store = createStore<State>({
             state.snackbarorder.show = true;
         },
         initSocket(state) {
-            state.socket.on("order.submitted", () => {
+            state.socket.on("order.assigned", () => {
                 store.commit('showSnackbarorder', {
                   message: {
                     id: '',
-                    status: 'submitted'
+                    status: 'assigned'
                   },
                   color: 'info',
                 });
@@ -94,6 +94,10 @@ export const store = createStore<State>({
                   color: 'info',
                 });
               });
+        },
+        connectSocket(state, token: string) {
+            state.socket.connect();
+            state.socket.emit('setClientId', token);
         },
         disconnectSocket(state) {
             state.socket.disconnect();
